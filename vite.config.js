f71b8f29ab68c488d7656a12c715ac49f72b0e58
svideoproekt-ship-it <svideoproekt@gmail.com>
2026-06-04 16:47:23 +0300

    Summary: feat: add prerendering for social media meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
@@ -13,18 +14,33 @@ export default defineConfig({
         description: 'Традиционные русские рецепты с историей',
         theme_color: '#E85D4E',
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
+    }),
+    prerender({
+      routes: [
+        '/',
+        '/recipe/1',
+        '/recipe/2',
+        '/recipe/3',
+        '/recipe/4',
+        '/recipe/5',
+        '/recipe/6',
+        '/recipe/7',
+        '/recipe/8',
+        '/recipe/9',
+        '/recipe/10',
+      ],
+      postProcess: (context) => {
+        context.route = context.route.replace(/\/$/, '')
+      }
     })
-  ]
+  ],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
