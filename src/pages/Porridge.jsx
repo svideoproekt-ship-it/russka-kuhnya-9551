c2c98b4f0a37d64de13bb2d693fb76f28c6f78c2
svideoproekt-ship-it <svideoproekt@gmail.com>
2026-06-07 12:03:25 +0300

    fix: remove ShareButtons from all category detail pages

--- src/pages/Porridge.jsx
+++ src/pages/Porridge.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { porridgeData } from '../data/porridgeData';
-import ShareButtons from '../components/ShareButtons';
 import './Porridge.css';
 
 function Porridge() {
