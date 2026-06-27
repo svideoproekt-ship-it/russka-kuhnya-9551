a783d8e2a201bd7855ba60e9180064a994ac8908
svideoproekt-ship-it <svideoproekt@gmail.com>
2026-06-27 17:37:58 +0300

    "feat: add Yandex Metrika correctly"

--- src/components/YandexMetrika.jsx
+++ src/components/YandexMetrika.jsx
@@ -2,53 +2,28 @@ import { useEffect } from 'react';
 
 const YandexMetrika = () => {
   useEffect(() => {
-    const counterId = 110199727;
-    
-    // Создаем скрипт Метрики
     const script = document.createElement('script');
     script.type = 'text/javascript';
+    script.async = true;
     script.innerHTML = `
-      (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {
-          if (document.scripts[j].src === r) { return; }
-        }
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${counterId}', 'ym');
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-      ym(${counterId}, 'init', {
-        ssr:true,
-        webvisor:true,
+      ym(110199727, 'init', {
         clickmap:true,
-        ecommerce:"dataLayer",
-        referrer: document.referrer,
-        url: location.href,
+        trackLinks:true,
         accurateTrackBounce:true,
-        trackLinks:true
+        webvisor:true
       });
     `;
     document.head.appendChild(script);
-    
-    // Создаем noscript fallback (пиксель для отслеживания без JS)
-    const noscript = document.createElement('noscript');
-    const div = document.createElement('div');
-    const img = document.createElement('img');
-    img.src = `https://mc.yandex.ru/watch/${counterId}`;
-    img.style.position = 'absolute';
-    img.style.left = '-9999px';
-    img.alt = '';
-    div.appendChild(img);
-    noscript.appendChild(div);
-    document.body.appendChild(noscript);
-    
+
     return () => {
-      // Очистка при размонтировании компонента
       if (script.parentNode) {
-        document.head.removeChild(script);
-      }
-      if (noscript.parentNode) {
-        document.body.removeChild(noscript);
+        script.parentNode.removeChild(script);
       }
     };
   }, []);
