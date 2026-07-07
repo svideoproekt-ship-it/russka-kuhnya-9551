9645207610d48e7b33efca145634c8ccc1af5f2d
svideoproekt-ship-it <svideoproekt@gmail.com>
2026-07-07 18:44:47 +0300

    Доюавлен импорт

--- src/pages/DrinksCategory.jsx
+++ src/pages/DrinksCategory.jsx
@@ -1,3 +1,4 @@
+import Breadcrumbs from '../components/Breadcrumbs';
 import SEO from '../components/SEO';
 import Breadcrumbs from '../components/Breadcrumbs';
 import React, { useState } from 'react';
